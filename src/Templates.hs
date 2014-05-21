f2f984119bde22159e971dcf0952d7e7cb5fd76b
Erlend Hamberg <erlend@hamberg.no>
2014-05-21 20:05:09 +0100

    Add Google Analytics

--- src/Templates.hs
+++ src/Templates.hs
@@ -49,6 +49,15 @@ base body = [shamlet|
       #{body}
       <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js">
       <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js">
+      <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-304186-3', '9m.no');
+        ga('send', 'pageview');
+      </script>
 |]
 
 header :: Markup
